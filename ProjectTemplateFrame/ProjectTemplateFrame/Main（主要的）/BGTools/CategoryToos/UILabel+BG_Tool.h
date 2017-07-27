bf712f4d0ad48b63dbec384155398b025096012d
huangxiongbiao12 <huangxiongbiao@huangxiongbiaodemac-mini.local>
2017-07-27 19:58:05 +0800

    增加label复制功能

--- ProjectTemplateFrame/ProjectTemplateFrame/Main（主要的）/BGTools/CategoryToos/UILabel+BG_Tool.h
+++ ProjectTemplateFrame/ProjectTemplateFrame/Main（主要的）/BGTools/CategoryToos/UILabel+BG_Tool.h
@@ -10,6 +10,9 @@
 
 @interface UILabel (BG_Tool)
 
+//在需要使用复制功能的时候只需要写这样一行代码：
+//xxxLabel.isCopyable = YES
+@property (nonatomic,assign) BOOL isCopyable;
 //根据标题适配高度
 -(double)fitHeightForTitle;
 -(double)fitWidthForTitle;
